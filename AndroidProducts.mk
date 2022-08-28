4b77dc28844e6a1605ceb9915eb3eba559e8c8cd
Sensei205 <maxim55miui11@gmail.com>
2022-08-28 18:24:24 +0300

    onclite: Bringup Superior

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/conquer_onclite.mk
+    $(LOCAL_DIR)/superior_onclite.mk
 
 COMMON_LUNCH_CHOICES := \
-    conquer_onclite-user \
-    conquer_onclite-userdebug \
-    conquer_onclite-eng
+    superior_onclite-user \
+    superior_onclite-userdebug \
+    superior_onclite-eng
