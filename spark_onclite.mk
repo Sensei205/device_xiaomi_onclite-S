752c21a1296469ce4778500dda993435de9cdafb
Sensei205 <maxim55miui11@gmail.com>
2022-08-30 02:06:13 +0300

    onclite: Build without gapps

--- spark_onclite.mk
+++ spark_onclite.mk
@@ -20,6 +20,7 @@ TARGET_BOOT_ANIMATION_RES := 720
 
 # Spark Stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := spark_onclite
