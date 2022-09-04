6123ee277be06cd5b938c485b16e68d33fa0f659
Sensei205 <maxim55miui11@gmail.com>
2022-09-04 17:47:20 +0300

    onclite: Bringup ForkLineageOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_onclite.mk
+    $(LOCAL_DIR)/lineage_onclite.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_onclite-user \
-    derp_onclite-userdebug \
-    derp_onclite-eng
+    lineage_onclite-user \
+    lineage_onclite-userdebug \
+    lineage_onclite-eng
--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 The Derp Project
+# Copyright (C) 2019-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,18 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Derp stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 720
 
-# Derp Stuff
+# LineageOS Stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
 WITH_GMS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_onclite
+PRODUCT_NAME := lineage_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
